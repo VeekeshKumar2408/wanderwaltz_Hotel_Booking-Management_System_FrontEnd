00fc9de2183e7b6e5b847ac77c3881c28204765b
VeekeshKumar2408 <singhofficial2408@gmail.com>
2024-01-29 12:09:17 +0530

    Booking logic done

---
+++ src/components/bookings/BookingSummary.jsx
@@ -0,0 +1,36 @@
+import moment from 'moment'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const BookingSummary = ({booking, payment, isFormValid, onConfirm}) => {
+    const checkInDate = moment(booking.checkInDate)
+    const checkOutDate = moment(booking.checkOutDate)
+    const numOfDays = checkOutDate.diff(checkInDate, "days")
+    const[isBookingConfirmed, setIsBookingConfirmed] = useState(false)
+    const[isProcessingPayment, setIsProcessingPayment] = useState(false)
+
+    const navigate = useNavigate( )
+
+    const handleConfirmBooking = () => {
+        setIsProcessingPayment(true)
+        setTimeout(() =>{
+            setIsProcessingPayment(false)
+            setIsBookingConfirmed(true)
+            onConfirm()
+        }, 3000)
+    }
+
+    useEffect(() => {
+      if(isBookingConfirmed){
+          navigate("/booking-success")
+      }
+    }, [isBookingConfirmed, navigate])
+    
+    return (
+    <div>
+
+    </div>
+   )
+}
+
+export default BookingSummary
--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -101,3 +101,51 @@ export async function getRoomById(roomId){
         throw new Error(`Error fetching room ${error.message}`)
     }
 }
+
+/**Booking new room */
+export async function bookRoom(roomId, booking){
+    try{
+        const response = await api.post(`/bookings/room/${roomId}/booking`, booking)
+        return response.data;
+    } catch(error){
+        if(error.response && error.response.data){
+            throw new Error(error.response.data)
+        }else{
+            throw new Error(`Error booking room: ${error.message}`)
+        }
+    }
+}
+
+/**Fetching all bookings */
+export async function getAllBookings(){
+    try{
+        const result = await api.get("/bookings/all-bookings")
+        return result.data
+    } catch(error){
+        throw new Error(`Error fetching bookings: ${error.message}`)
+    }
+}
+
+/**Search Booking by Confirmation code */
+export async function getBookingByConfirmationCode(confirmationCode){
+    try{
+        const result = await api.get(`/bookings/confirmation/${confirmationCode}`)
+        return result.data;
+    } catch(error){
+        if(error.response && error.response.data){
+            throw new Error(error.response.data)
+        } else{
+            throw new Error(`Error finding booking : ${error.message}`)
+        }
+    }
+}
+
+/**Cancel booking */
+export async function cancelBooking(bookingId){
+    try{
+        const result = await api.delete(`/bookings/booking/${bookingId}/delete`)
+        return result.data
+    } catch(error){
+        throw new Error(`Error cancelling booking : ${error.message}`)
+    }
+}
